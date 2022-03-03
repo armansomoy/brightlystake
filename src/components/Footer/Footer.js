458c3be72b391fea9a294ea1b08ce74f266d9088
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-03-03 13:19:37 +0600

    pushing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,13 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <div id="footer">
+                <div className="container">
+                    <div className="row text-center">
+                        <p>If you encounter an issue please report to us</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
