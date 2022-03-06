cf864de66d37cd9ed738171ecf1b1dd2d8e40299
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-03-06 15:22:56 +0600

    done

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,7 +2,6 @@ import React from "react";
 import Delegator from "./delegator/delegator";
 import Moonbeam from "./moonbeam/moonbeam";
 import Individual from "./individual/individual";
-import DelegatorData from "./delegator/delegatorDark";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 const Body = () => {
@@ -11,7 +10,6 @@ const Body = () => {
       <Route path="/" exact component={Delegator} />
       <Route path="/moonbeam" exact component={Moonbeam} />
       <Route path="/individual" exact component={Individual} />
-      <Route path="/delegatordark" exact component={DelegatorData} />
       <Redirect to='/' />
     </Switch>
   );
